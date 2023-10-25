89c8fc42c26108b6088c2edd9f6f06fd8200b6d8
Mengwai Chan <chanmengwai1@gmail.com>
2023-10-25 13:30:36 -0400

    fixed destination name

--- busRoutes.py
+++ busRoutes.py
@@ -1,6 +1,11 @@
 import busStop
 from route import Route
 
+w145 = busStop.BusStop(1, "W145", 40.82377614314247, -73.94502568555461, 691, "St Nicholas Ave") #691 St Nicholas Ave
+nac = busStop.BusStop(2, "NAC", 40.819557163853155, -73.94991793531442, 201, "Convent Ave") # 201 The City College of New York
+w125 = busStop.BusStop(3, "W125", 40.8103721597239, -73.95278450679731, 284, "St Nicholas Ave") # 284 St Nicholas Ave # W 124th St
+intermediate = busStop.BusStop(4, "intermediate", 40.810976077358795, -73.95405670678701)
+
 class BusRoutes:
     def __init__(self, datetime, name, serialnumber, lat, lng, streetaddress, streetname):
         self.datetime = datetime
@@ -14,15 +19,17 @@ class BusRoutes:
         self.duration = None
         self.w125 = False # if true it passed w125, false is heading to w125
         self.w145 = False 
-        self.destlat = busStop.nac.stopLat
-        self.destlng = busStop.nac.stopLng
-        self.interlat = None
-        self.interlng = None
+        self.nextStop = None
+        self.intermediate = None
 
     def fetchRoute(self):
         bus_route = Route()
 
-        response_data = bus_route.getRoute(self.lat, self.lng, self.destlat, self.destlng)
+        destlat = self.nextStop.getLat()
+        destlng = self.nextStop.getLng()
+
+
+        response_data = bus_route.getRoute(self.lat, self.lng, destlat, destlng)
 
         self.distance = response_data['routes'][0]['distanceMeters'] 
         self.duration = response_data['routes'][0]['duration']
@@ -36,34 +43,28 @@ class BusRoutes:
         if int(self.streetaddress) >= 690 and self.streetname == "St Nicholas Ave": #145
             self.w145 = True
             self.w125 = False
-            self.destlat = busStop.nac.stopLat
-            self.destlng = busStop.nac.stopLng
+            self.nextStop = nac
             return
         if int(self.streetaddress) < 210 and self.streetname == "Convent Ave": # CCNY
             if self.w145: # going to w125
-                self.destlat = busStop.w125.stopLat
-                self.destlng = busStop.w125.stopLng
-                self.interlat = busStop.intermediate.stopLat
-                self.interlng = busStop.intermediate.stopLng
+                self.nextStop = w125
+                self.intermediate = intermediate
                 return
             else: # going to w145
-                self.destlat = busStop.w145.stopLat
-                self.destlng = busStop.w145.stopLng
+                self.nextStop = w145
                 return
         if int(self.streetaddress) <= 300 and self.streetname == "St Nicholas Ave": #125
             self.w145 = False
             self.w125 = True
-            self.destlat = busStop.nac.stopLat
-            self.destlng = busStop.nac.stopLng
+            self.nextStop = nac
             return
         
-    def setdest(self, destlat, destlng, interlat, interlng):
-        self.destat = destlat
-        self.destlng = destlng
-        self.interlat = interlat
-        self.interlng = interlng   
-    
+    def getLastDest(self, route):
+        self.nextStop = route.nextStop
+        self.intermediate = route.intermediate
+
     def to_json(self):
+        stop = self.nextStop.getName()
         return {
             "datetime":self.datetime,
             "name":self.name,
@@ -73,5 +74,6 @@ class BusRoutes:
             "streetAddress" : self.streetaddress,
             "streetName" : self.streetname,
             "distance" : self.distance,
-            "duration" : self.duration
+            "duration" : self.duration,
+            "nextStop" : stop
         }
